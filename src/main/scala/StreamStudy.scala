7737bb1e2ee7240736b9022aa54118d6d702ff75
QWYNG <ikusawasi@gmail.com>
2020-04-08 19:06:55 +0900

    impl lazy StreamStudy

--- src/main/scala/StreamStudy.scala
+++ src/main/scala/StreamStudy.scala
@@ -1,8 +1,18 @@
 trait StreamStudy[+A] {
 
-  def headOption: Option[A] = ???
+  def headOption: Option[A] = {
+    this match {
+      case EmptyStream => None
+      case Cons(h, _)  => Some(h())
+    }
+  }
 
-  def tail: StreamStudy[A] = ???
+  def tail: StreamStudy[A] = {
+    this match {
+      case EmptyStream => throw new NoSuchMethodError()
+      case Cons(_, t)  => t()
+    }
+  }
 
 }
 
@@ -11,8 +21,11 @@ case object EmptyStream extends StreamStudy[Nothing]
 case class Cons[+A](h: () => A, t: () => StreamStudy[A]) extends StreamStudy[A]
 
 object StreamStudy {
-
-  def cons[A](h: => A, t: => StreamStudy[A]): StreamStudy[A] = ???
+  def cons[A](h: => A, t: => StreamStudy[A]): StreamStudy[A] = {
+    lazy val headResult = h
+    lazy val tailResult = t
+    Cons(() => headResult, () => tailResult)
+  }
 
   def empty[A]: StreamStudy[A] = EmptyStream
 
